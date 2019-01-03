fd289ee8b79f25231d187d250e3d420fdde0adfa
Andre <andre.pbo@gmail.com>
2019-01-03 13:37:42 -0300

    Create CalculateLine.java

---
+++ CalculateLine.java
@@ -0,0 +1,15 @@
+public class CalculateLine extends ConsoleProgram {
+	public void run() {
+		println( "This program calculates y coordinates for a line." );
+		int x = 0;
+		int slope = readInt( "Enter slope (m): " );
+		int intercept = readInt( "Enter intercept (b): " );
+		while( x >= 0 ) {
+			x = readInt( "Enter x: " );
+			if( x >= 0 ) {
+				int y = (slope * x) + intercept;
+				println( "f(" + x + ") = " + y );
+			}
+		}
+	}
+}
